0f2f4a77a7e42976e4fc61dc3f9761dc6ad399cb
Sahan Ramanayake <68191711+SahanRama@users.noreply.github.com>
2025-04-08 14:53:51 -0400

    adding the finanl code

--- set_email.py
+++ set_email.py
@@ -1,22 +1,51 @@
 from email.message import EmailMessage
 import ssl
 import smtplib
+from datetime import datetime
+import calendar
 
 email_sender = "p8on.reminder@gmail.com"
+email_password = ""
+email_recipients = []
 
 email_server = "smtp.gmail.com"
 email_port = 465
 
 subject = "Sending test email from python"
-body = """
-Sending out the python email.
 
-"""
 def main():
-      send_email(subject =subject,body=body,sender= email_sender,recipients=email_recipients,password=email_password)
+      send_email(subject =subject,body=get_body(),sender= email_sender,recipients=email_recipients,password=email_password)
 
-def set_body():
-      TODO
+
+def get_body():
+      
+      return f"""
+Hello Everyone,
+      
+Am reaching out regarding your time sheet for the period {get_date()}. We haven’t received notification of your submission yet.
+      
+Kindly submit your timesheets as soon as possible to avoid any last-minute issues with the tool.
+      
+Please note, our team will reach out to you via teams if necessary."""
+
+ 
+def get_date():
+      # Find todays date
+      today = datetime.today()
+      year = today.year
+      month = today.month
+      day = today.day
+      
+
+      # find the last date of the month
+      last_day = calendar.monthrange(year,month)[1]
+
+      if day < 15:
+            date_obj = datetime(year, month, 15)
+      else:
+            date_obj = datetime(year, month, last_day)
+
+      return date_obj.strftime("%b-%d-%Y")
 
 def send_email(subject, body, sender, recipients, password):
     em = EmailMessage()
@@ -31,7 +60,6 @@ def send_email(subject, body, sender, recipients, password):
     
             server.login(sender, password)
             server.sendmail(sender,recipients,em.as_string())
-            server.quit() 
 
 if __name__ == "__main__":
     main()
